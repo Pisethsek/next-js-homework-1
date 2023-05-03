60f282c1e61f8d60d12892e15fb0b517aa18464b
= <pisethsek9899@gmail.com>
2023-05-03 14:57:15 +0700

    my hw

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
+import '@/styles/bootstrap.min.css'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    
+    <Component {...pageProps} />
+    </>
+  )
 }
